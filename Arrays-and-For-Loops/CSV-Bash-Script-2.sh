62a3db8f30c262c1b86fd5ae098c311c3af2fcee
Elizabeth Watt <bethwatt87@gmail.com>
2026-05-21 17:12:56 -0400

    Update CSV-Bash-Script-2.sh

--- Arrays-and-For-Loops/CSV-Bash-Script-2.sh
+++ Arrays-and-For-Loops/CSV-Bash-Script-2.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+ 
+csv_file="arrays_table.csv"
+ 
+column_0=($(cut -d "," -f 1 $csv_file | tail -n +2))
+column_1=($(cut -d "," -f 2 $csv_file | tail -n +2))
+column_2=($(cut -d "," -f 3 $csv_file | tail -n +2))
+ 
+echo "Displaying the first column:"
+echo "${column_0[@]}"
