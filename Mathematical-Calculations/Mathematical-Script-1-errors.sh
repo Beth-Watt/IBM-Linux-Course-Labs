d8c81ee60e1cb0e2d467d5f474284bba152da13c
Elizabeth Watt <bethwatt87@gmail.com>
2026-05-21 18:20:21 -0400

    Create Mathematical-Script-1-errors.sh

---
+++ Mathematical-Calculations/Mathematical-Script-1-errors.sh
@@ -0,0 +1,11 @@
+#/bin/bash calculations_script.sh
+echo -n "Enter an integer: "
+read n1
+echo -n "Enter another integer: "
+read n2
+ 
+sum=$(($n1+$n2))
+products=$(($n1*$nw))
+ 
+echo "The sum of $n1 and $n2 is $sum"
+echo "The product of $n1 and $n2 is $product."
